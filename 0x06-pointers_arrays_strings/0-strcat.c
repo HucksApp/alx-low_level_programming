abd9ca0dc0da8c8b60cc569a763a774da3919b91
HucksApp <hucksapp@gmail.com>
2023-09-21 11:02:27 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,16 +14,16 @@ char *_strcat(char *dest, char *src)
 	i = 0;
 	j = 0;
 
-	while (*(dest + i) != '\0')
+	while (dest[i] != '\0')
 		i++;
 
-	while (*(src + j) != '\0')
+	while (src[j] != '\0')
 	{
-		*(dest + i) = src[j];
+		dest[i] = src[j];
 		j++;
 		i++;
 	}
-	*(dest + i) = '\0';
+
+	dest[i] = '\0';
 
 	return (dest);
-};
