370dc9289a68cf8cb9d74c9540cce3a147897dfe
HucksApp <hucksapp@gmail.com>
2023-09-25 12:36:27 -0700

    locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: string to search
+ * @c: char to find
+ *
+ * Return: a pointer to the first occurrence of the character
+ * c in the string s, or NULL if the character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	int count = 0;
+
+	for (; s[count] != '\0', count++)
+	{
+		if (s[count] == c)
+			return (s + count);
+	}
+	return (NULL)
+
+}
