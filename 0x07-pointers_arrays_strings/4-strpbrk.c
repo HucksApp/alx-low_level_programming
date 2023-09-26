3cfa264dfc7ebabed7bae255ff8822802658485e
HucksApp <hucksapp@gmail.com>
2023-09-25 17:07:47 -0700

    searches a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,28 +1,27 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-* _strspn - Gets the length of a prefix substring.
-* @s: String where substring will look.
-* @accept: Substring of accepted chars.
-* Return: Length of occurrence.
-*/
+ * _strpbrk - bytes
+ * @s: pointer to char
+ * @accept: pointer to char
+ * Return: NULL
+ */
 
-unsigned int _strspn(char *s, char *accept)
+char *_strpbrk(char *s, char *accept)
 {
-	unsigned int c = 0;
-	char *t = accept;
+	int i;
 
-	while (*s++)
+	while (*s)
 	{
-		while (*accept++)
-			if (*(s - 1) == *(accept - 1))
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
 			{
-				c++;
-				break;
+				return (s);
 			}
-		if (!(*--accept))
-			break;
-		accept = t;
+		}
+		s++;
 	}
-	return (c);
+	return (NULL);
 }
