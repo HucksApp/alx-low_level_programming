d06180ec208b6e72bbb8f3ec124ddd26aaa50afa
HucksApp <hucksapp@gmail.com>
2023-12-07 08:33:05 -0800

    q2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,65 @@
+#include "lists.h"
+
+/**
+ * createNode - creates new node
+ * @num: number
+ * Return: pointer to the new node
+*/
+
+
+
+dlistint_t *createNode(int num)
+{
+	dlistint_t *node;
+
+	node = malloc(sizeof(dlistint_t));
+	node->n = num;
+	node->next = NULL;
+	node->prev = NULL;
+	if (!node)
+		return (NULL);
+	return (node);
+}
+
+
+
+
+
+/**
+ * add_dnodeint -  appends node
+ * @head: dlistint_t type head
+ * @n: number
+ * Return: pointer to added node
+*/
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *curr, *node;
+
+	/* head pointer is pointing to NULL i.e no head */
+	if (!head)
+		return (NULL);
+
+	/* head is pointing to NULL i.e no node in list*/
+	if (!(*head))
+	{
+		/*iadd node as first node*/
+		node = createNode(n);
+		if (!node)
+			return (NULL);
+
+		(*head) = node;
+		return (node);
+	}
+
+	/* list is not empty, add node as 1st node*/
+	curr = *head;
+	node = createNode(n);
+
+	if (!node)
+		return (NULL);
+	*head = node;
+	node->next = curr;
+	curr->prev = node;
+	return (node);
+}
