0348b3a218045fde4afd6c4d86e46190553f3449
HucksApp <hucksapp@gmail.com>
2023-12-07 06:38:07 -0800

    q0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,4 @@
-**
+/**
  * print_dlistint - prints the data in
  * dlistint_t type
  * @h: dlistint_t type head
@@ -8,12 +8,11 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	const dlistint_t *curr;
-	int iter;
+        const dlistint_t *curr;
+        int iter;
 
-	curr = h;
-	for (iter = 0; curr; curr = curr->next, iter++)
-		printf("%d\n", curr->n);
-	return (iter);
+        curr = h;
+        for (iter = 0; curr; curr = curr->next, iter++)
+                printf("%d\n", curr->n);
+        return (iter);
 }
-
