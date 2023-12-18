c0698881595d988b5d6e895d3b9119b1b6947c3b
Hucksapp <hucksapp@gmail.com>
2023-12-18 09:24:38 -0800

    hack

---
+++ 0x18-dynamic_libraries/hack.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+int printf(const char *format, ...)
+{
+    write(1, "9 8 10 24 75 - 9\n", 18);
+    write(1,"Congratulations, you win the Jackpot !\n", 40);
+    exit(EXIT_SUCCESS);
+}
+
+int main(void)
+{
+    printf("me");
+    return (0);
+}
