2e2d9ad5d1217370266ea6d96342ee01d1b8bd46
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2023-12-23 23:36:27 -0800

    new

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,41 +11,37 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long int index, iter;
 	hash_node_t *new;
-	char *value_copy;
-	unsigned long int index, i;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-		return (0);
-
-	value_copy = strdup(value);
-	if (value_copy == NULL)
+	if (!ht || !key || !value || *key == '\0')
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	for (i = index; ht->array[i]; i++)
-	{
+
+	for (iter = index; ht->array[iter]; iter++)
+	{/*check if key exits in list */
 		if (strcmp(ht->array[i]->key, key) == 0)
-		{
+		{/*replace key value with new value*/
 			free(ht->array[i]->value);
-			ht->array[i]->value = value_copy;
+			ht->array[i]->value = strdup(value);
 			return (1);
 		}
 	}
-
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-	{
-		free(value_copy);
+	/* key is new */
+	new = (hash_node_t *)malloc(sizeof(hash_node_t));
+	if (!new)
 		return (0);
-	}
+	
 	new->key = strdup(key);
-	if (new->key == NULL)
+	new->value = strdup(value);
+	if (!new->key || !new->value)
 	{
 		free(new);
 		return (0);
 	}
-	new->value = value_copy;
+
+
 	new->next = ht->array[index];
 	ht->array[index] = new;
 
