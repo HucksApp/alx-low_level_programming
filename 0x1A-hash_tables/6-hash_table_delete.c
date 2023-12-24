162b55f9dc8317e519453d580ca6af8620612a4f
HucksApp <hucksapp@gmail.com>
2023-12-24 00:10:36 -0800

    new

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,21 +10,23 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *node, *tmp;
 	unsigned long int i;
 
+	/*free array elements or lists*/
 	for (i = 0; i < ht->size; i++)
-	{
+	{/*if node list head is not empty*/
 		if (ht->array[i] != NULL)
-		{
+		{/*advance and free*/
 			node = ht->array[i];
 			while (node != NULL)
 			{
-				tmp = node->next;
+				tmp = node->next; /*reference to nxt node */
 				free(node->key);
 				free(node->value);
 				free(node);
-				node = tmp;
+				node = tmp; /*move to nxt node*/
 			}
 		}
 	}
+	/*free array and table memory*/
 	free(head->array);
 	free(head);
 }
