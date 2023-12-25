a67dd786ded46fd2417f841585927e71fb7dd440
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2023-12-25 07:29:20 -0800

    100_get

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -142,18 +142,21 @@ char *shash_table_get(const shash_table_t *ht, const char *key)
 	shash_node_t *node;
 	unsigned long int index;
 
-	if (ht == NULL || key == NULL || *key == '\0')
+	if(!ht || !key  || *key == '\0' )
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	if (index >= ht->size)
+	if(index >= ht->size)
 		return (NULL);
 
 	node = ht->shead;
-	while (node != NULL && strcmp(node->key, key) != 0)
+	while (node)
+	{
+		if(strcmp(node->key, key) == 0)
+			return (node->value);
 		node = node->snext;
-
-	return ((node == NULL) ? NULL : node->value);
+	}
+	return (NULL);
 }
 
 /**
