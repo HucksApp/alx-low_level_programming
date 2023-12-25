a4aac6ddea308102078456bfd1159d5eecdb663d
HucksApp <hucksapp@gmail.com>
2023-12-25 07:31:31 -0800

    get_100

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -142,17 +142,17 @@ char *shash_table_get(const shash_table_t *ht, const char *key)
 	shash_node_t *node;
 	unsigned long int index;
 
-	if(!ht || !key  || *key == '\0' )
+	if (!ht || !key  || *key == '\0')
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	if(index >= ht->size)
+	if (index >= ht->size)
 		return (NULL);
 
 	node = ht->shead;
 	while (node)
 	{
-		if(strcmp(node->key, key) == 0)
+		if (strcmp(node->key, key) == 0)
 			return (node->value);
 		node = node->snext;
 	}
