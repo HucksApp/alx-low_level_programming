27ae61ee68a30d32e9f996030fe1f8f90a7e6701
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2023-12-25 06:56:19 -0800

    100_new

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -49,75 +49,82 @@ shash_table_t *shash_table_create(unsigned long int size)
  */
 int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 {
-	shash_node_t *new, *tmp;
-	char *value_copy;
+	shash_node_t *new, *tmp, *prev;
 	unsigned long int index;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-		return (0);
 
-	value_copy = strdup(value);
-	if (value_copy == NULL)
-		return (0);
+	if(!ht || !key || !value || *key == '\0')
+		return(0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	tmp = ht->shead;
-	while (tmp)
-	{
-		if (strcmp(tmp->key, key) == 0)
-		{
-			free(tmp->value);
-			tmp->value = value_copy;
-			return (1);
+	if(ht->shead)
+	{/*the nodes head is not empty*/
+		tmp = ht->shead;
+		while(tmp)
+		{/*check if key exists*/
+			if(strcmp(tmp->key,key) == 0)
+			{/* replace */
+				free(tmp->value);
+				tmp->value = strdup(value);
+				return (1);
+			}
+			tmp =tmp->snext;
 		}
-		tmp = tmp->snext;
 	}
-
-	new = malloc(sizeof(shash_node_t));
-	if (new == NULL)
+	/* its new*/
+	new = (shash_node_t *)malloc(sizeof(shash_node_t));
+	if(!new)
 	{
-		free(value_copy);
-		return (0);
-	}
-	new->key = strdup(key);
-	if (new->key == NULL)
-	{
-		free(value_copy);
 		free(new);
 		return (0);
 	}
-	new->value = value_copy;
-	new->next = ht->array[index];
-	ht->array[index] = new;
-
-	if (ht->shead == NULL)
+	new->key = strdup(key);
+	new->value = strdup(value);
+	if(!new->key || !new->value)
 	{
-		new->sprev = NULL;
-		new->snext = NULL;
-		ht->shead = new;
+		free(new);
+		return (0);
+	}
+	new->next = ht->array[index]; /*insert at top*/
+	ht->array[index] = new;
+	if(!ht->shead)
+	{/* no head node yet, definately no tail*/
+		new->sprev = NULL; /* mark start */
+		new->snext = NULL; /* mark start */
+		ht->shead =  new;
 		ht->stail = new;
 	}
-	else if (strcmp(ht->shead->key, key) > 0)
-	{
+	else if(strcmp(ht->shead->key, new->key) > 0)
+	{/* Ascending order: new key is smaller than old key */
 		new->sprev = NULL;
 		new->snext = ht->shead;
 		ht->shead->sprev = new;
-		ht->shead = new;
+		ht->shead = new; /*new is new shead  */
 	}
 	else
-	{
+	{/* new key is bigger than the old key*/
+		/*new is new stail  or just insert new in ranked position*/
 		tmp = ht->shead;
-		while (tmp->snext != NULL && strcmp(tmp->snext->key, key) < 0)
-			tmp = tmp->snext;
-		new->sprev = tmp;
-		new->snext = tmp->snext;
-		if (tmp->snext == NULL)
-			ht->stail = new;
+		while(tmp && strcmp(tmp->key, key) < 0)
+		{
+			prev = tmp; /*rev to prev node before last node or null*/
+			tmp = tmp->snext; /*move to insertion point*/
+		}
+		if(tmp)
+		{ /* new is not tail, just insert*/
+			new->sprev = tmp->sprev;
+			tmp->sprev->snext  = new;
+			new->snext = tmp;
+			tmp->sprev = new;
+		}
 		else
-			tmp->snext->sprev = new;
-		tmp->snext = new;
-	}
-
+		{/* new is  tail*/
+			prev->snext = new;
+			new->sprev = prev;
+			new->snext = NULL;
+			ht->stail = new;
+		}
+    }
 	return (1);
 }
 
